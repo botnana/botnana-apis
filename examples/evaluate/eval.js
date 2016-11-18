9e60dcf50e64531a1f96da358644091001b08db9
Sirius Wu <ccwu660601@gmail.com>
2016-11-18 21:34:00 +0800

    added config.save() to example.

--- examples/evaluate/eval.js
+++ examples/evaluate/eval.js
@@ -3,7 +3,8 @@
 var WebSocket = require('ws');
 var botnana = {};
 
-botnana.ws = new WebSocket('ws://192.168.7.2:3012');
+// botnana.ws = new WebSocket('ws://192.168.7.2:3012');
+botnana.ws = new WebSocket('ws://localhost:3012');
 
 botnana.ws.on('open', function open() {
     botnana.is_connected = true;
@@ -21,9 +22,17 @@ botnana.motion = {
     }
 }
 
+botnana.config = {
+    save: function() {
+        var json = "{\"spec_version\":\"0.0.1\",\"target\":\"config\",\"command\":\"save\"}";
+        botnana.ws.send(json);
+    }
+}
+
 function test_botnana() {
     var script = "words";
     botnana.motion.evaluate(script);
+    botnana.config.save();
 }
 
 setTimeout(test_botnana, 500);
