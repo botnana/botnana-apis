005189ad80c008235cf3fddef2871f6d34571d99
Sirius Wu <ccwu660601@gmail.com>
2016-11-22 12:02:42 +0800

    implemented set_device.

--- examples/evaluate/eval.js
+++ examples/evaluate/eval.js
@@ -16,22 +16,52 @@ botnana.ws.on('message', function(data, flags) {
 
 botnana.motion = {
     evaluate: function(script) {
-        var json = "{\"spec_version\":\"0.0.1\",\"target\":\"motion\",\"command\":\"evaluate\",\"arguments\":{\"script\":\""
-        + script + "\"}}";    
-        botnana.ws.send(json);
+        var json = {
+            spec_version: "0.0.1",
+            target: "motion",
+            command: "evaluate",
+            arguments: {
+                script: script
+            }
+        };
+        console.log(JSON.stringify(json));
+        botnana.ws.send(JSON.stringify(json));
     }
 }
 
 botnana.config = {
     save: function() {
-        var json = "{\"spec_version\":\"0.0.1\",\"target\":\"config\",\"command\":\"save\"}";
-        botnana.ws.send(json);
+        var json = {
+            spec_version: "0.0.1",
+            target: "config",
+            command: "save"
+        };
+        botnana.ws.send(JSON.stringify(json));
+    },
+    set_device: function(args) {
+        var json = {
+            spec_version: "0.0.1",
+            target: "config",
+            command: "set_device",
+            arguments: args
+        };
+        botnana.ws.send(JSON.stringify(json));
     }
 }
 
 function test_botnana() {
     var script = "words";
     botnana.motion.evaluate(script);
+    botnana.config.set_device({
+        position: 1,
+        tag: "homing_method",
+        value: 33
+    });
+    botnana.config.set_device({
+        position: 1,
+        tag: "homing_speed_1",
+        value: 18000
+    });
     botnana.config.save();
 }
 
