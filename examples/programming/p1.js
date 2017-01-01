59201c6801ef0607475b023e8770ef4ad25ae6d7
Sirius Wu <ccwu660601@gmail.com>
2017-01-01 20:50:52 +0800

    added examples p2, p3, p4.

--- examples/programming/p1.js
+++ examples/programming/p1.js
@@ -3,15 +3,16 @@
 
 var botnana = require('../../index')
 
-function test_programming_api() {
+function test() {
     // Show reponse data
     botnana.debug_level = 0;
     botnana.on("ready", function() {
         // Programming API                
-        var p1 = new botnana.Program("p1");
-//        var s1 = p1.ethercat.slave(1);
-//        s1.hm();
+        var Program = botnana.Program;
+        // Program p1
+        var p1 = new Program("p1");
         p1.deploy();
+        // Run Program p1
         setTimeout(function() {
             p1.run();
         }, 1000);
@@ -19,4 +20,4 @@ function test_programming_api() {
     botnana.start("ws://192.168.7.2:3012");
 }
 
-test_programming_api();
+test();
---
+++ examples/programming/p2.js
@@ -0,0 +1,28 @@
+
+"use strict"
+
+var botnana = require('../../index')
+
+function test() {
+    // Show reponse data
+    botnana.debug_level = 0;
+    botnana.on("ready", function() {
+        // Programming API                
+        var Program = botnana.Program;
+        // Program p2
+        var p2 = new Program("p2");
+        var s1 = p2.ethercat.slave(1);
+        s1.hm();
+        s1.go();
+        s1.move_to(30000);
+        s1.go();
+        p2.deploy();
+        // Run Program p1
+        setTimeout(function() {
+            p2.run();
+        }, 1000);
+    })
+    botnana.start("ws://192.168.7.2:3012");
+}
+
+test();
