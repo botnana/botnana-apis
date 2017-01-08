7d2c700b31946ba49d7cb47621218e07abf7881b
Sirius Wu <ccwu660601@gmail.com>
2017-01-09 05:53:25 +0800

    added p5 for IO and CONTROL Programming API.

---
+++ examples/programming/p5.js
@@ -0,0 +1,47 @@
+"use strict"
+
+var botnana = require('../../index')
+
+function test() {
+    // Show reponse data
+    botnana.debug_level = 0;
+    botnana.on("ready", function() {
+        // Programming API
+        var Program = botnana.Program;
+        // Program p5
+        var p5 = new Program("p5");
+        var douts = p5.ethercat.slave(1);
+        var dins = p5.ethercat.slave(2);
+        var aouts = p5.ethercat.slave(3);
+        var ains = p5.ethercat.slave(4);
+        aouts.disable_aout(1);
+        ains.disable_ain(1);
+        aouts.set_aout(1, 0);
+        douts.set_dout(1, 0);
+        p5.ms(1000);
+        aouts.enable_aout(1);
+        ains.enable_ain(1);
+        p5.DO(0, 10);
+            p5.ms(1000);
+            p5.IF(dins.din(1));
+                douts.set_dout(1, 0);
+            p5.ELSE()
+                douts.set_dout(1, 1);
+            p5.THEN();
+        p5.LOOP();
+        p5.BEGN();
+            p5.ms(1000);
+            p5.IF(ains.ain(1).GT(1000));
+                p5.aouts.set_aout(1, 0);
+            p5.ELSE();
+                p5.aouts.set_aout(1, 10000);
+            p5.THEN();
+        p5.AGAIN();
+        p5.deploy();        
+        // Run Program p1
+        botnana.on("deployed", function() { p5.run(); });
+    })
+    botnana.start("ws://192.168.7.2:3012");
+}
+
+test();
