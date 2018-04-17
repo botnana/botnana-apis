0f4327811c1932f2d982c56509a56488e9068aed
alvin <alvinchang@mapacode.tw>
2018-04-17 16:45:19 +0800

    Alvin: 增加 empty 範例

--- botnanac/examples/empty.c
+++ botnanac/examples/empty.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include "botnana.h"
 
-
 void handle_meaasge (const char * src)
 {
 	printf("C handle_meaasge: %s \n", src);
-
+	
 }
-
 
 int main() {
 
 	struct Botnana * botnana = connect_to_botnana("192.168.7.2:3012", handle_meaasge);
-	motion_evaluate(botnana, "words");
-
+	
 	while (1)
 	{
 		sleep(1);
@@ -21,3 +18,4 @@ int main() {
 	return 0;
 
 }
+
