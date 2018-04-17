2b19b5040ecfd9c3021e54dc88ac64132acd7c36
alvin <alvinchang@mapacode.tw>
2018-04-17 14:22:35 +0800

    Alvin: 在 rust 增加 mtconnect 封包解析，但會當機，之後再處理。

--- botnanac/src/botnana.h
+++ botnanac/src/botnana.h
@@ -2,8 +2,9 @@ struct Botnana;
 
 void hello_botnana ();
 
-struct Botnana * connect_to_botnana(char * address, void  (* fn)(char * str));
+struct Botnana * connect_to_botnana(char * address, void (* fn)(char * str));
 
 void motion_evaluate (struct Botnana * desc, char * cmd);
 
+void attach_function_to_event(struct Botnana * desc, char * event, unsigned int count, void  (* fn)(char * str));
 
--- botnanac/src/botnana.rs
+++ botnanac/src/botnana.rs
@@ -1,15 +1,19 @@
+extern crate libc;
 use std::thread;
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::ffi::CStr;
 use std::os::raw::c_char;
 use std::str;
 use websocket::{ClientBuilder, OwnedMessage};
 use websocket::result::WebSocketError;
+use std::collections::HashMap;
 
 /// Botnana
 #[repr(C)]
 pub struct Botnana {
     sender: Option<mpsc::Sender<OwnedMessage>>,
+    handlers: Arc<Mutex<HashMap<&'static str, Vec<Box<Fn(&str) + Send>>>>>,
+    handler_counters: Arc<Mutex<HashMap<&'static str, Vec<u32>>>>,
 }
 
 /// hello botnana (test function)
@@ -41,9 +45,14 @@ pub extern "C" fn connect_to_botnana(
             let (sender, receiver) = mpsc::channel();
             let botnana = Botnana {
                 sender: Some(sender),
+                handlers: Arc::new(Mutex::new(HashMap::new())),
+                handler_counters: Arc::new(Mutex::new(HashMap::new())),
             };
 
             let (mut rx, mut tx) = n.connect_insecure().unwrap().split().unwrap();
+
+            let handlers = Arc::clone(&botnana.handlers);
+            let handler_counters = Arc::clone(&botnana.handler_counters);
 
             // 處理 botnana 傳過來的訊息
             thread::spawn(move || {
@@ -52,6 +61,10 @@ pub extern "C" fn connect_to_botnana(
                         Ok(msg) => {
                             match msg {
                                 OwnedMessage::Text(m) => {
+                                    botnana_handle_message(&handlers, &handler_counters, &m);
+                                    //    let cstr = CString::from_slice(mystr.as_bytes());
+                                    //    cstr.as_slice().as_ptr()
+
                                     msg_processor(&m);
                                 }
                                 _ => panic!("Invalid message {:?}", msg),
@@ -77,11 +90,89 @@ pub extern "C" fn connect_to_botnana(
     }
 }
 
+fn botnana_handle_message(
+    handlers: &Arc<Mutex<HashMap<&'static str, Vec<Box<Fn(&str) + Send>>>>>,
+    handler_counters: &Arc<Mutex<HashMap<&'static str, Vec<u32>>>>,
+    msg: &str,
+) {
+    if msg != "" {
+        let lines: Vec<&str> = msg.split("\n").collect();
+        let mut handlers = handlers.lock().unwrap();
+        let mut handler_counters = handler_counters.lock().unwrap();
+        for line in lines {
+            let mut r: Vec<&str> = line.split("|").collect();
+            let mut index: u32 = 0;
+            let mut event = "";
+            for e in r {
+                println!("{}", e);
+                if index % 2 == 0 {
+                    event = e;
+                } else {
+                    let mut remove_list: Vec<usize> = Vec::new();
+                    let mut idx = 0;
+                    let mut counter_exist = false;
+                    match handlers.get(event) {
+                        Some(handler) => {
+                            let counter = match handler_counters.get_mut(event) {
+                                Some(c) => c,
+                                None => {
+                                    panic!("handler_counters.get_mut");
+                                }
+                            };
+                            counter_exist = true;
+                            for h in handler {
+                                h(e);
+                                if counter[idx] > 0 {
+                                    counter[idx] -= 1;
+                                    if counter[idx] == 0 {
+                                        remove_list.push(idx);
+                                        idx = idx + 1;
+                                    }
+                                }
+                            }
+                        }
+                        None => {}
+                    }
+                    if counter_exist {
+                        let counters = handler_counters.get_mut(event).unwrap();
+                        let handlers = handlers.get_mut(event).unwrap();
+
+                        for i in remove_list {
+                            handlers.remove(i);
+                            counters.remove(i);
+                        }
+                    }
+                }
+                index = index + 1;
+            }
+        }
+    }
+}
+
+fn times<F>(
+    handlers: &Arc<Mutex<HashMap<&'static str, Vec<Box<Fn(&str) + Send>>>>>,
+    handler_counters: &Arc<Mutex<HashMap<&'static str, Vec<u32>>>>,
+    event: &'static str,
+    count: u32,
+    handler: F,
+) where
+    F: Fn(&str) + Send + 'static,
+{
+    let mut handlers = handlers.lock().unwrap();
+    let mut handler_counters = handler_counters.lock().unwrap();
+
+    let h = handlers.entry(event).or_insert(Vec::new());
+    let hc = handler_counters.entry(event).or_insert(Vec::new());
+
+    h.push(Box::new(handler));
+    hc.push(count);
+}
+
 /// Send message
 #[no_mangle]
 pub fn send_message(botnana: Box<Botnana>, msg: &str) {
     let botnana = Box::into_raw(botnana);
-    let s = unsafe { &(*botnana).sender };
+    let s = unsafe { &(*botnana).sender.clone() };
     match s {
         &Some(ref sender) => {
             let msg = OwnedMessage::Text(msg.to_string());
@@ -100,8 +191,25 @@ pub fn motion_evaluate(botnana: Box<Botnana>, script: *const c_char) {
         assert!(!script.is_null());
         str::from_utf8(CStr::from_ptr(script).to_bytes()).unwrap()
     };
-
     let msg = r#"{"jsonrpc":"2.0","method":"motion.evaluate","params":{"script":""#.to_owned()
         + script + r#""}}"#;
     send_message(botnana, &msg.to_owned());
+}
+
+/// attach function to event
+#[no_mangle]
+pub fn attach_function_to_event(
+    botnana: Box<Botnana>,
+    event: *const c_char,
+    count: libc::uint32_t,
+    call: fn(&str),
+) {
+    let event = unsafe {
+        assert!(!event.is_null());
+        str::from_utf8(CStr::from_ptr(event).to_bytes()).unwrap()
+    };
+
+    let handlers = Arc::clone(&botnana.handlers);
+    let handler_counters = Arc::clone(&botnana.handler_counters);
+    times(&handlers, &handler_counters, event, count, call);
 }
