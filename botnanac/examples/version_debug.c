f2b5728d754be238ea7d7423413c8fdb41808599
alvin <alvinchang@mapacode.tw>
2018-04-23 10:48:36 +0800

    Alvin: 遺漏 examples/version_debug.c

---
+++ botnanac/examples/version_debug.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "botnana.h"
+
+unsigned int version_count = 0;
+unsigned int message_count = 0;
+
+void handle_meaasge (const char * src)
+{
+	message_count = message_count + 1;
+	printf("handle_meaasge (%u): %s \n", message_count, src);
+
+}
+
+
+void handle_version (const char * src)
+{
+	version_count = version_count + 1;
+	printf("handle_version (%u): %s \n", version_count, src);
+
+}
+
+
+int main() {
+	unsigned int count = 0;
+	struct Botnana * botnana = botnana_connect("192.168.7.2", handle_meaasge);
+	botnana_attach_event(botnana, "version", 12, handle_version);
+    botnana_enable_debug (botnana);
+	while (1)
+	{
+		count = count + 1;
+		if (count > 20){
+			botnana_disable_debug (botnana);
+		}
+		botnana_get_version(botnana);
+
+		sleep(1);
+	}
+	return 0;
+
+}
