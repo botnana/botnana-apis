a8e76170bdcd5fb265fcb2b08c6e2a1616709bf4
alvin <alvinchang@mapacode.tw>
2018-04-23 09:41:35 +0800

    Alvin: 修改 example/slave_setting

--- botnanac/examples/slave_setting.c
+++ botnanac/examples/slave_setting.c
@@ -1,58 +1,60 @@
 #include <stdio.h>
 #include "botnana.h"
-#include "json_api.h"
+
+unsigned int message_count = 0;
 
 void handle_meaasge (const char * src)
 {
-	printf("C handle_meaasge: %s \n", src);
+	message_count = message_count + 1;
+	printf("handle_meaasge (%u): %s \n", message_count, src);
 
 }
 
 
 int main() {
 
-	struct Botnana * botnana = connect_to_botnana("192.168.7.2:3012", handle_meaasge);
-	get_slave(botnana, 1);
+	struct Botnana * botnana = botnana_connect("192.168.7.2", handle_meaasge);
+	botnana_get_slave(botnana, 1);
 	sleep(1);
-	set_slave(botnana, 1, "homing_method", 33);
+	botnana_set_slave(botnana, 1, "homing_method", 33);
 	sleep(1);
-	set_slave(botnana, 1, "home_offset", 0);
+	botnana_set_slave(botnana, 1, "home_offset", 0);
 	sleep(1);
-	set_slave(botnana, 1, "homing_speed_1", 100000);
+	botnana_set_slave(botnana, 1, "homing_speed_1", 100000);
 	sleep(1);
-	set_slave(botnana, 1, "homing_speed_2", 1000);
+	botnana_set_slave(botnana, 1, "homing_speed_2", 1000);
 	sleep(1);
-	set_slave(botnana, 1, "homing_acceleration", 2000000);
+	botnana_set_slave(botnana, 1, "homing_acceleration", 2000000);
 	sleep(1);
-	set_slave(botnana, 1, "profile_velocity", 200000);
+	botnana_set_slave(botnana, 1, "profile_velocity", 200000);
 	sleep(1);
-	set_slave(botnana, 1, "profile_acceleration", 2000000);
+	botnana_set_slave(botnana, 1, "profile_acceleration", 2000000);
 	sleep(1);
-	set_slave(botnana, 1, "profile_deceleration", 2000000);
+	botnana_set_slave(botnana, 1, "profile_deceleration", 2000000);
 	sleep(1);
-	get_slave_diff(botnana, 1);
+	botnana_get_slave_diff(botnana, 1);
 	sleep(1);
-	set_slave(botnana, 1, "homing_method", 35);
+	botnana_set_slave(botnana, 1, "homing_method", 35);
 	sleep(1);
-	get_slave_diff(botnana, 1);
+	botnana_get_slave_diff(botnana, 1);
 	sleep(1);
-	set_slave_config(botnana, 1, "homing_method", 33);
+	botnana_set_slave_config(botnana, 1, "homing_method", 33);
 	sleep(1);
-	set_slave_config(botnana, 1, "home_offset", 0);
+	botnana_set_slave_config(botnana, 1, "home_offset", 0);
 	sleep(1);
-	set_slave_config(botnana, 1, "homing_speed_1", 100000);
+	botnana_set_slave_config(botnana, 1, "homing_speed_1", 100000);
 	sleep(1);
-	set_slave_config(botnana, 1, "homing_speed_2", 1000);
+	botnana_set_slave_config(botnana, 1, "homing_speed_2", 1000);
 	sleep(1);
-	set_slave_config(botnana, 1, "homing_acceleration", 123456);
+	botnana_set_slave_config(botnana, 1, "homing_acceleration", 5000);
 	sleep(1);
-	set_slave_config(botnana, 1, "profile_velocity", 1234567);
+	botnana_set_slave_config(botnana, 1, "profile_velocity", 100000);
 	sleep(1);
-	set_slave_config(botnana, 1, "profile_acceleration", 1234567);
+	botnana_set_slave_config(botnana, 1, "profile_acceleration", 500000);
 	sleep(1);
-	set_slave_config(botnana, 1, "profile_deceleration", 1234567);
+	botnana_set_slave_config(botnana, 1, "profile_deceleration", 500000);
 	sleep(1);
-	save_config(botnana);
+	botnana_save_config(botnana);
 	while (1)
 	{
 		sleep(1);
--- botnanac/src/botnana.h
+++ botnanac/src/botnana.h
@@ -23,5 +23,20 @@ void botnana_motion_evaluate(struct Botnana * desc, const char * script);
 // get version
 void botnana_get_version(struct Botnana * desc);
 
+// get slave
+void botnana_get_slave(struct Botnana * desc, uint32_t position);
+
+// set slave
+void botnana_set_slave(struct Botnana * desc, uint32_t position, const char * tag, int32_t value);
+
+// get slave diff.
+void botnana_get_slave_diff(struct Botnana * desc, uint32_t position);
+
+// set slave config
+void botnana_set_slave_config(struct Botnana * desc, uint32_t position, const char * tag, int32_t value);
+
+// save configuration
+void botnana_save_configure(struct Botnana * desc);
+
 
 #endif
