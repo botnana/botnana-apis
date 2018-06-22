d12bb09b64b56716982540d16752a84772c642f8
alvin <alvinchang@mapacode.tw>
2018-06-22 11:40:33 +0800

    windows 不產生 dll 檔

--- botnanac/build.bat
+++ botnanac/build.bat
@@ -9,7 +9,5 @@ cargo clean
 cargo update
 cargo build --release --target=i686-pc-windows-msvc
 copy target\i686-pc-windows-msvc\release\botnana.lib lib\i686-pc-windows-msvc\botnana.lib
-copy target\i686-pc-windows-msvc\release\botnana.dll lib\i686-pc-windows-msvc\botnana.dll
 cargo build --release --target=x86_64-pc-windows-msvc
 copy target\x86_64-pc-windows-msvc\release\botnana.lib lib\x86_64-pc-windows-msvc\botnana.lib
-copy target\x86_64-pc-windows-msvc\release\botnana.dll lib\x86_64-pc-windows-msvc\botnana.dll
