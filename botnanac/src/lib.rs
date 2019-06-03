e7a28435dc923a9df9d9d0e3bb11803332eb3cb2
alvin <alvinchang@mapacode.tw>
2019-06-03 15:23:37 +0800

    將 C 函式庫分離為 Rust 與 C 介面兩部份

--- botnanac/src/lib.rs
+++ botnanac/src/lib.rs
@@ -1,10 +1,6 @@
+extern crate botnanars;
 extern crate libc;
-extern crate serde_json;
-extern crate url;
-extern crate ws;
 
 pub mod botnana;
 pub mod json_api;
 pub mod program;
-
-pub use botnana::Botnana;
