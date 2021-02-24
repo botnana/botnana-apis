ac273c4005666ab7e0e18e96497c3dd40b4e7ee2
Sirius Wu <ccwu660601@mapacode.tw>
2021-02-24 20:02:01 +0800

    Fix example, change callback's type to extern "C" fn.

--- examples/words.rs
+++ examples/words.rs
@@ -8,7 +8,7 @@ static mut IS_OPENED: bool = false;
 const NULL: *mut c_void = 0 as (*mut c_void);
 
 /// On ws open
-fn on_ws_open(_ptr: *mut c_void, msg: *const c_char) {
+extern "C" fn on_ws_open(_ptr: *mut c_void, msg: *const c_char) {
     let message = unsafe {
         assert!(!msg.is_null());
         str::from_utf8(CStr::from_ptr(msg).to_bytes()).unwrap()
@@ -20,7 +20,7 @@ fn on_ws_open(_ptr: *mut c_void, msg: *const c_char) {
 }
 
 /// On ws error
-fn on_ws_error(_ptr: *mut c_void, msg: *const c_char) {
+extern "C" fn on_ws_error(_ptr: *mut c_void, msg: *const c_char) {
     let message = unsafe {
         assert!(!msg.is_null());
         str::from_utf8(CStr::from_ptr(msg).to_bytes()).unwrap()
@@ -32,7 +32,7 @@ fn on_ws_error(_ptr: *mut c_void, msg: *const c_char) {
 }
 
 /// On ws message
-fn on_ws_message(_ptr: *mut c_void, msg: *const c_char) {
+extern "C" fn on_ws_message(_ptr: *mut c_void, msg: *const c_char) {
     let message = unsafe {
         assert!(!msg.is_null());
         str::from_utf8(CStr::from_ptr(msg).to_bytes()).unwrap()
