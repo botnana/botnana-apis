f4794e664b4a96dcf52336d2775d4fc9b25ca33b
Sirius Wu <ccwu660601@mapacode.tw>
2022-05-17 18:28:03 +0800

    Check boundaries of modbus inputs.

--- examples/modbus-tcp-client.rs
+++ examples/modbus-tcp-client.rs
@@ -17,19 +17,22 @@ fn main() {
             interval.tick().await;
             if botnana.is_mb_connected() {
                 botnana.mb_table().update();
-                info!("bit[10001]={:?}, bit[12000]={:?}, i16[30001]={:?}, i16[30000+N]={:?}, u16[30001]={:?}, u16[30000+N]={:?}, i32[30001]={:?}, i32[30000+N]={:?}, i32[30000+N-1]={:?}, u32[30001]={:?}, u32[30000+N]={:?}, u32[30000+N-1]={:?}",
+                info!("bit[10001]={:?}, bit[10000+N*16]={:?}, bit[10000+N*16+1]={:?}, i16[30001]={:?}, i16[30000+N]={:?}, i16[30000+N+1]={:?}, u16[30001]={:?}, u16[30000+N]={:?}, u16[30000+N+1]={:?}, i32[30001]={:?}, i32[30000+N-1]={:?}, i32[30000+N]={:?}, u32[30001]={:?}, u32[30000+N-1]={:?}, u32[30000+N]={:?}",
                     botnana.mb_bit(10001),
-                    botnana.mb_bit(12000),
+                    botnana.mb_bit(10000+MB_BLOCK_SIZE*16),
+                    botnana.mb_bit(10000+MB_BLOCK_SIZE*16+1),
                     botnana.mb_i16(30001),
                     botnana.mb_i16(30000+MB_BLOCK_SIZE),
+                    botnana.mb_i16(30000+MB_BLOCK_SIZE+1),
                     botnana.mb_u16(30001),
                     botnana.mb_u16(30000+MB_BLOCK_SIZE),
+                    botnana.mb_u16(30000+MB_BLOCK_SIZE+1),
                     botnana.mb_i32(30001),
-                    botnana.mb_i32(30000+MB_BLOCK_SIZE),
                     botnana.mb_i32(30000+MB_BLOCK_SIZE-1),
+                    botnana.mb_i32(30000+MB_BLOCK_SIZE),
                     botnana.mb_u32(30001),
-                    botnana.mb_u32(30000+MB_BLOCK_SIZE),
                     botnana.mb_u32(30000+MB_BLOCK_SIZE-1),
+                    botnana.mb_u32(30000+MB_BLOCK_SIZE),
                 );
             }
         }
