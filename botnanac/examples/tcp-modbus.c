b63f5e241a92e960c39b46ebe8b1ae5021375627
howard <loveconan006@mapacode.tw>
2022-09-12 15:07:36 +0800

    在tcp-modbus測試heatbeat相關功能

--- botnanac/examples/tcp-modbus.c
+++ botnanac/examples/tcp-modbus.c
@@ -1,15 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "botnana.h"
+void log_cb (void * data, const char * src)
+{
+    printf("log|%s\n", src);
+}
 
+void error_cb (void * data, const char * src)
+{
+    printf("error|%s\n", src);
+}
+// 處理 WebSocket on_open
+void on_ws_open_cb (void * data, const char * src)
+{
+    int * open = (int *) data;
+    *open = 1;
+}
+
+// 處理 WebSocket 連線異常
+void on_ws_error_cb (void * data, const char * src)
+{
+    int * open = (int *) data;
+    *open = 0;
+    printf("WS client error: %s\n", src);
+    exit(1);
+}
 int main()
 {
+    FILE *fp;
+    fp = fopen("../../motion/examples/hmpppv/drive-pp.fs", "r");
+    char buf[1000];
+
+    int ws_open = 0;
     // connect to motion server
     printf("Creating botnana client to 192.168.7.2\n");
     struct Botnana * botnana = botnana_new("192.168.7.2");
+
+    botnana_set_tag_cb(botnana, "log", 0, NULL, log_cb);
+    botnana_set_tag_cb(botnana, "error", 0, NULL ,error_cb);
+    botnana_set_on_open_cb(botnana, (void *) & ws_open, on_ws_open_cb);
+    botnana_set_on_error_cb(botnana, (void *) & ws_open, on_ws_error_cb);
+
+    botnana_connect(botnana);
+    while (ws_open == 0)
+    {
+        sleep(1);
+    }
+    while (fgets(buf, 1000, fp) != NULL){
+        printf("%s\n", buf);
+        script_evaluate(botnana, buf);
+    }
+    script_evaluate(botnana, "start-task1");
     printf("Connecting to modbus server at 192.168.7.2\n");
     botnana_mb_connect(botnana);
     printf("Modbus server connected\n");
+    script_evaluate(botnana, "start-4drive");
+    script_evaluate(botnana, "1 1 reset-fault");
+    sleep(1);
+    botnana_mb_update(botnana);
+    script_evaluate(botnana, "1 1 drive-off");
+    sleep(1);
+    botnana_mb_update(botnana);
+    script_evaluate(botnana, "1 1 drive-on");
+    sleep(1);
+    botnana_mb_update(botnana);
+
+    script_evaluate(botnana, "2 mb-heart-beat-watchdog-dur!");
+    script_evaluate(botnana, "1 mb-heart-beat!");
+    sleep(1);
+    botnana_mb_update(botnana);
+    script_evaluate(botnana, "2 mb-heart-beat!");
+    sleep(1);
+    botnana_mb_update(botnana);
+
     // 等待連線成功
     while (1)
     {
